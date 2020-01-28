2765a8100a692a8f40f960f870a638d4ce80b875
Anton Engman <antengman@gmail.com>
2020-01-28 14:01:00 +0100

    Added simple arithmetic function

--- hello.c
+++ hello.c
@@ -8,10 +8,18 @@ MODULE_AUTHOR("ANTON");
 MODULE_DESCRIPTION("HELLO WORLD");
 MODULE_VERSION("0.1");
 
+static int AddIntegers(int a, int b)
+{
+	int sum = a + b;
+	return sum;
+}
+
 static int __init hello_init(void)
 {
+	int sum = AddIntegers(1,1);
 	printk(KERN_ALERT "Hello world!\n");
 	printk(KERN_ALERT "This process is named \"%s\" and has the pid %i\n", current->comm, current->pid);
+	printk(KERN_ALERT "AddIntegers() returned %d\n", sum);
 	return 0;
 }
 
