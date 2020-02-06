e770d9a7ad8eb4641ddced8c8d0713b1078762de
Anton Engman <antengman@gmail.com>
2020-02-06 13:29:25 +0100

    Add read() function

--- test.c
+++ test.c
@@ -2,10 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
 
 int main()
 {
-	int f = open("/dev/scull", O_WRONLY);
+	int f = open("/dev/scull", O_RDWR);
+	char buff[1000];
+	size_t nbytes;
+	ssize_t bytes_read;
+	int fd;
+
 	if(f < 0)
 	{
 		printf("Fail\n");
@@ -13,6 +19,8 @@ int main()
 	else
 	{
 		printf("Succeeded\n");
+		nbytes = sizeof(buff);
+		bytes_read = read(fd, buff, nbytes);
 	}
 	return 0;
 }
